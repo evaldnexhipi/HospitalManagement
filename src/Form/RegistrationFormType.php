7ad82dde3a495a698d31e0b4eb68bd451080bcc0
kevindule10 <51861182+kevinthecoder10@users.noreply.github.com>
2020-05-02 18:14:49 +0200

    Update RegistrationFormType.php

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,18 +3,18 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Doctrine\DBAL\Types\DateType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class RegistrationFormType extends AbstractType
 {
@@ -32,6 +32,8 @@ class RegistrationFormType extends AbstractType
             ->add('email',EmailType::class,[
                 'required'=>true,
                 'label'=>false,
+                'invalid_message'=>'Emaili ekziston',
+                'csrf_message'=>'Emaili ekziston'
             ])
             ->add('password', RepeatedType::class, [
                 'type'=>PasswordType::class,
@@ -41,6 +43,7 @@ class RegistrationFormType extends AbstractType
                 'required'=>true,
                 'first_options'=>['label'=>false],
                 'second_options'=>['label'=>false],
+                'invalid_message'=>'Fjalekalimet nuk perputhen'
             ])
             ->add('gender',ChoiceType::class,[
                 'choices'=>[
@@ -60,11 +63,27 @@ class RegistrationFormType extends AbstractType
             ])
             ->add('telephone',TelType::class,[
                 'required'=>false,
-                'label'=>false
+                'label'=>false,
+                'invalid_message'=>'Format i gabuar'
             ])
             ->add('address',TextType::class,[
                 'required'=>false,
                 'label'=>false,
+            ])
+            ->add('imageFilename',FileType::class,[
+                'label'=>false,
+                'mapped'=>false,
+                'required'=>false,
+                'constraints'=>[
+                    new File([
+                        'maxSize'=>'8192k',
+                        'mimeTypes'=>[
+                            'image/jpeg',
+                            'image/jpg',
+                        ],
+                        'mimeTypesMessage' => 'Ju lutemi te vendosni nje foto',
+                    ])
+                ],
             ])
         ;
     }
