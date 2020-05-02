78ebc30384005e037252b4bf7a13793c7f927cdc
kevindule10 <51861182+kevinthecoder10@users.noreply.github.com>
2020-05-02 18:14:34 +0200

    Update SecurityController.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\LoginFormAuthenticator;
@@ -9,6 +10,8 @@ use App\Service\TokenGenerator;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -46,18 +49,76 @@ class SecurityController extends AbstractController
     /**
      * @Route("/register", name="app_register")
      */
+<<<<<<< HEAD
     public function register(Request $request, TokenGenerator $tokenGenerator, UserPasswordEncoderInterface $encoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator,  Mailer $mailer): Response
     {
+=======
+    public function register(Request $request,
+                             UserPasswordEncoderInterface $passwordEncoder,
+                             GuardAuthenticatorHandler $guardHandler,
+                             LoginFormAuthenticator $authenticator,
+                             EntityManagerInterface $entityManager)
+    {
+        $user = new User();
+        $client = new Client();
+>>>>>>> b8dcd1da404d3c82a80333099d6e50d2e119df9e
         $form = $this->createForm(RegistrationFormType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+<<<<<<< HEAD
             $user = $form->getData();
             // encode the plain password
             $user->setPassword($encoder->encodePassword($user, $user->getPassword()));
             $token = $tokenGenerator->generateToken();
             $user->setToken($token);
             $user->setIsActive(false);
+=======
+
+            $user->setFirstName(ucfirst(strtolower($form->get('firstName')->getData())));
+            $user->setLastName(ucfirst(strtolower($form->get('lastName')->getData())));
+
+            $user->setEmail($form->get('email')->getData());
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+            $user->setGender($form->get('gender')->getData());
+            $user->setBirthday($form->get('birthday')->getData());
+
+            if (!is_numeric($form->get('telephone')->getData())){
+                $form->addError(new FormError('Telefoni te permbaje numra','telephone','telephone','telephone'));
+            }
+
+            $user->setTelephone($form->get('telephone')->getData());
+            $user->setAddress($form->get('address')->getData());
+
+            $imageFile = $form->get('imageFilename')->getData();
+            if($imageFile){
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(),PATHINFO_FILENAME);
+                $newFilename = $originalFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                try{
+                    $imageFile->move(
+                        $this->getParameter('user_images_directory'),
+                        $newFilename
+                    );
+                }
+                catch(FileException $e){
+                    dd($e);
+                }
+                $user->setImageFilename($newFilename);
+            }
+
+            $user->setClient($client);
+            $client->setUser($user);
+
+            $entityManager->persist($user);
+            $entityManager->persist($client);
+            $entityManager->flush();
+>>>>>>> b8dcd1da404d3c82a80333099d6e50d2e119df9e
 
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
@@ -72,6 +133,7 @@ class SecurityController extends AbstractController
         }
         return $this->render('registration/register.html.twig.', [
             'registrationForm' => $form->createView(),
+            'errors'=>$form->getErrors()
         ]);
     }
     /**
